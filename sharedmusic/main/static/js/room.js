747b60fff19fe43aca8ed6a7d63f056d84850c7a
erikgab01 <erikgab01@gmail.com>
2022-04-20 20:44:13 +0500

    Added button and field to get youtube url

--- sharedmusic/main/static/js/room.js
+++ sharedmusic/main/static/js/room.js
@@ -16,9 +16,9 @@ function connect() {
         );
     };
 
-  roomSocket.onclose = (e) => {
-    console.log(e.reason);
-  }
+    roomSocket.onclose = (e) => {
+        console.log(e.reason);
+    };
 
     roomSocket.onmessage = (e) => {
         let data = JSON.parse(e.data);
@@ -37,3 +37,89 @@ function connect() {
 }
 
 connect();
+
+const urlField = document.getElementById("url-field");
+const audio_tag = document.getElementById("youtube");
+
+function changeSong() {
+    const url = urlField.value;
+    let audio_streams = {};
+    fetch("http://localhost:8080/" + url).then((response) => {
+        if (response.ok) {
+            response.text().then((data) => {
+                console.log(data);
+                var regex =
+                    /(?:ytplayer\.config\s*=\s*|ytInitialPlayerResponse\s?=\s?)(.+?)(?:;var|;\(function|\)?;\s*if|;\s*if|;\s*ytplayer\.|;\s*<\/script)/gmsu;
+
+                data = data.split("window.getPageData")[0];
+                data = data.replace("ytInitialPlayerResponse = null", "");
+                data = data.replace(
+                    "ytInitialPlayerResponse=window.ytInitialPlayerResponse",
+                    ""
+                );
+                data = data.replace(
+                    "ytplayer.config={args:{raw_player_response:ytInitialPlayerResponse}};",
+                    ""
+                );
+
+                var matches = regex.exec(data);
+                var data =
+                    matches && matches.length > 1
+                        ? JSON.parse(matches[1])
+                        : false;
+
+                console.log(data);
+
+                var streams = [];
+
+                if (data.streamingData) {
+                    if (data.streamingData.adaptiveFormats) {
+                        streams = streams.concat(
+                            data.streamingData.adaptiveFormats
+                        );
+                    }
+
+                    if (data.streamingData.formats) {
+                        streams = streams.concat(data.streamingData.formats);
+                    }
+                } else {
+                    return false;
+                }
+
+                streams.forEach(function (stream, n) {
+                    var itag = stream.itag * 1,
+                        quality = false;
+                    console.log(stream);
+                    switch (itag) {
+                        case 139:
+                            quality = "48kbps";
+                            break;
+                        case 140:
+                            quality = "128kbps";
+                            break;
+                        case 141:
+                            quality = "256kbps";
+                            break;
+                        case 249:
+                            quality = "webm_l";
+                            break;
+                        case 250:
+                            quality = "webm_m";
+                            break;
+                        case 251:
+                            quality = "webm_h";
+                            break;
+                    }
+                    if (quality) audio_streams[quality] = stream.url;
+                });
+
+                console.log(audio_streams);
+
+                audio_tag.src =
+                    audio_streams["256kbps"] ||
+                    audio_streams["128kbps"] ||
+                    audio_streams["48kbps"];
+            });
+        }
+    });
+}
