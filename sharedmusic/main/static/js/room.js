397825f7dd660e4f84f517b1e852410545989a3f
erikgab01 <erikgab01@gmail.com>
2022-05-17 20:31:03 +0500

    Change host button reworked
    - Now it is a dropdown with various options

--- sharedmusic/main/static/js/room.js
+++ sharedmusic/main/static/js/room.js
@@ -570,7 +570,16 @@ function updateUserList(users) {
             `<li>` + `<div class="online"></div>` + `<div class="username">${user.username}</div>` + `</li>`
         );
         if (user.username != hostUsername && username == hostUsername) {
-            let changeHostButton = $(`<button onClick="changeHost('${user.username}')">Change</button>`);
+            //let changeHostButton = $(`<button onClick="changeHost('${user.username}')">Change</button>`);
+            let changeHostButton = $(`
+            <div class="dropdown">
+                <button class="dropbtn"><i class="fa-regular fa-square-caret-down"></i></button>
+                <div class="dropdown-content">
+                <a href="javascript:void(0)" onclick="changeHost('${user.username}')">Change host</a>
+                <a href="javascript:void(0)">Kick user</a>
+                <a href="javascript:void(0)">Ban user</a>
+                </div>
+            </div>`);
             node.append(changeHostButton);
         }
         if (user.username == hostUsername) {
@@ -696,6 +705,5 @@ function savePerms() {
             permissions: permissions,
         })
     );
-    // Also modal button should be visible only to host
     modal.hide();
 }
